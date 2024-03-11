84e54af9322173c2dd4fb25f043fe664c26dc25a
mimiko76 <tema.levin2016@gmail.com>
2024-03-11 19:06:57 +0300

    End commit

--- Certification/src/main/java/org/example/Main.java
+++ Certification/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //kk
         WorkWithData data = new WorkWithData();
         data.NomberOfWords();
         data.LongestWord();
